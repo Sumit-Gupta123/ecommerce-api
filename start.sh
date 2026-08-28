2b1daacdfe5385f8fb9c96cac07fe2d61adb70bc
Sumit-Gupta123 <vishnugupta9571@gmail.com>
2026-08-28 05:44:02 +0530

    Clear route and config caches on startup

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # 1. Discover packages and cache config (now that env vars are loaded)
 php artisan package:discover --ansi
 php artisan config:cache
-php artisan route:cache
+php artisan route:clear
 
 # 2. Run migrations automatically
 php artisan migrate --force
