d545429e9f4b45f91c242db8daa521d52035737d
Sumit-Gupta123 <vishnugupta9571@gmail.com>
2026-08-28 06:28:22 +0530

    cors issue

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://ecommerce-api-wjow.onrender.com'],
+    'allowed_origins' => ['https://ecommerce-client-alpha-mocha.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
